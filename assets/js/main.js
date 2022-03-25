fb5ec0f559c5fcf7a4d14e7436049486e58bd02d
ANDO Andriamalala <integrateur@rova-caviar.com>
2022-03-25 14:25:09 +0300

    animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,3 +233,74 @@
   });
 
 })()
+
+
+// parallax animation
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.header_background, .moove_back').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+function moveImg() {
+  x += (lFollowX - x) * 1 / 60;
+  y += (lFollowY - y) * 1 / 60;
+  
+  translate = 'translate(' + (x * -1)  + 'px, ' + (y * -1) + 'px)';
+
+
+  $('#animated_logo, .moove_top').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveImg);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
+moveImg();
+
+// typeritter 
+var i = 0;
+// var A = 0;
+		var tag = document.getElementById("text");
+		var html = document.getElementById("text").innerHTML;
+		var attr = tag.setAttribute("data", html);
+		var txt = tag.getAttribute("data");
+		var speed = 100;
+
+		function typeWriter() {
+		  if (i <= txt.length) {
+		    document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
+		    i++;
+		    setTimeout(typeWriter, speed);
+		  }
+		    //console.log(document.getElementById("text").innerHTML);
+		}
+
+typeWriter();
