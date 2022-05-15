f56bc38ac5fa255f8753ce1933588367cd9fd364
ANDO Andriamalala <andriamalala5056@gmail.com>
2022-05-15 15:42:46 +0300

    lotties

--- assets/js/main.js
+++ assets/js/main.js
@@ -286,21 +286,19 @@
 // moveImg();
 
 // typeritter 
-var i = 0;
-// var A = 0;
-		var tag = document.getElementById("text");
-		var html = document.getElementById("text").innerHTML;
-		var attr = tag.setAttribute("data", html);
-		var txt = tag.getAttribute("data");
-		var speed = 100;
+// var i = 0;
+// 		var tag = document.getElementById("text");
+// 		var html = document.getElementById("text").innerHTML;
+// 		var attr = tag.setAttribute("data", html);
+// 		var txt = tag.getAttribute("data");
+// 		var speed = 100;
 
-		function typeWriter() {
-		  if (i <= txt.length) {
-		    document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
-		    i++;
-		    setTimeout(typeWriter, speed);
-		  }
-		    //console.log(document.getElementById("text").innerHTML);
-		}
+// 		function typeWriter() {
+// 		  if (i <= txt.length) {
+// 		    document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
+// 		    i++;
+// 		    setTimeout(typeWriter, speed);
+// 		  }
+// 		}
 
-typeWriter();
+// typeWriter();
